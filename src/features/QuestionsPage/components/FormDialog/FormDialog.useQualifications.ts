431d6ffac273bbd7062a4e96925a8a904c9f6914
Kichiyaki <xyztojajestem@gmail.com>
2021-04-17 15:32:43 +0200

    FormDialog.useQualifications.ts - increase limit

--- src/features/QuestionsPage/components/FormDialog/FormDialog.useQualifications.ts
+++ src/features/QuestionsPage/components/FormDialog/FormDialog.useQualifications.ts
@@ -3,12 +3,17 @@ import { useQuery } from '@apollo/client';
 import { QUERY_QUALIFICATIONS } from './queries';
 import { Query, QueryQualificationsArgs } from 'libs/graphql/types';
 
+const LIMIT = 1000;
+
 const useQualifications = () => {
   const { data, loading, refetch } = useQuery<
     Pick<Query, 'qualifications'>,
     QueryQualificationsArgs
   >(QUERY_QUALIFICATIONS, {
     fetchPolicy: 'cache-and-network',
+    variables: {
+      limit: LIMIT,
+    },
   });
   const qualifications = useMemo(() => data?.qualifications.items ?? [], [
     data,
