7e33fbecf42e78a0adc06b056e9a5fb06db779ce
Kichiyaki <xyztojajestem@gmail.com>
2021-06-10 18:31:55 +0200

    FormDialog refactor

---
+++ src/features/QualificationsPage/components/FormDialog/FormDialog.useSuggestions.ts
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import { useApolloClient } from '@apollo/client';
+import { useDebounce } from 'react-use';
+import { QUERY_PROFESSIONS } from './queries';
+import { Profession, Query, QueryProfessionsArgs } from 'libs/graphql/types';
+
+const useSuggestions = () => {
+  const [suggestions, setSuggestions] = useState<Profession[]>([]);
+  const [isLoadingSuggestions, setIsLoadingSuggestions] = useState<boolean>(
+    false
+  );
+  const [search, setSearch] = useState<string>('');
+  const client = useApolloClient();
+
+  const loadSuggestions = async (search: string) => {
+    setIsLoadingSuggestions(true);
+    try {
+      const { data } = await client.query<
+        Pick<Query, 'professions'>,
+        QueryProfessionsArgs
+      >({
+        query: QUERY_PROFESSIONS,
+        fetchPolicy: 'no-cache',
+        variables: {
+          filter: {
+            nameIEQ: '%' + search + '%',
+          },
+          limit: 10,
+        },
+      });
+      if (data.professions?.items) {
+        setSuggestions(data.professions.items);
+      }
+    } catch (e) {}
+    setIsLoadingSuggestions(false);
+  };
+
+  useDebounce(
+    () => {
+      loadSuggestions(search);
+    },
+    500,
+    [search]
+  );
+
+  return {
+    isLoadingSuggestions,
+    suggestions,
+    setSearch,
+    search,
+  };
+};
+
+export default useSuggestions;
--- src/features/QualificationsPage/components/FormDialog/types.ts
+++ src/features/QualificationsPage/components/FormDialog/types.ts
@@ -1,12 +1,8 @@
 import { Profession, QualificationInput } from 'libs/graphql/types';
 
-export type ExtendedProfession = Profession & {
-  disabled?: boolean;
-};
-
 export type Input = Omit<
   QualificationInput,
   'associateProfession' | 'dissociateProfession'
 > & {
-  professions: ExtendedProfession[];
+  professions: Profession[];
 };
